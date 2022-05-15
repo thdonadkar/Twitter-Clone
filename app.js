b696ff2d7278d439a27cfef1417e2409061b8934
Tejas Rajendra Donadkar <72977141+thdonadkar@users.noreply.github.com>
2022-05-15 16:44:10 +0530

    Setting Up Template Engine PUG

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const port = 3003;
 const server = app.listen(port, (req, res) => {
     console.log("server listening on port 🟢 "+ port);
 })
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+
+    var payload = {
+        pageTitle: "Home"
+    }
+
+    res.status(200).render("home",payload);
+})
